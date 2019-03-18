a61b4c68d378f372e14087e823e3e0642893c1bd
Brian Bohe <brianbohe@gmail.com>
2019-03-18 16:12:03 -0700

    Editing CodeGen::genProduceReg header comments

--- src/jit/codegen.h
+++ src/jit/codegen.h
@@ -1013,6 +1013,8 @@ protected:
 
 #endif // !defined(_TARGET_64BIT_)
 
+    // Do liveness update for register produced by the current node in codegen after
+    // code has been emitted for it.
     void genProduceReg(GenTree* tree);
     void genUnspillRegIfNeeded(GenTree* tree);
     regNumber genConsumeReg(GenTree* tree);
